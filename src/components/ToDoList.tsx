b77eb2da529e663ac1177e722bb21270c6928f20
Edward Vera <edwardismael1@gmail.com>
2023-09-24 14:54:17 -0300

    cambios

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -16,7 +16,7 @@ export const ToDoList = () => {
         setListaTareas(tareas => tareas.filter((_, i) => i !== index))
     }
     return (
-    <div>
+    <div className="flex">
         <h1>Lista de Tareas</h1>
         <div>
             <input type="text"
